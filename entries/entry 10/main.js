1779e58206913abf7e0676af9f54fbe231f33be0
jinnkim <kimh000@newschool.edu>
2024-11-18 02:30:46 -0500

    chnage

--- entries/entry 10/main.js
+++ entries/entry 10/main.js
@@ -10,6 +10,6 @@ function forgotPassword() {
   const securityAnswer = prompt("What is your biggest secrete you ever kept?");
   if (securityAnswer !== null) {
     // Redirect to a different page (you can replace the URL below with your desired page)
-    window.location.href = "https://www.example.com";
+    window.location.href = "https://oursecretdairy.github.io/jinny/entries/entry1/";
   }
 }
